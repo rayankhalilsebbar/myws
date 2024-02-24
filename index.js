788d93ce72917f79edb051fac1c6fe71ee6fa5cc
Rayan Khalil Sebbar <rayankh23@gmail.com>
2024-02-24 16:57:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ const copyToClipboard = async () => {
       // Optional: Display an error message to the user
     }
   };
+
+var string = "Hello! I'm Rayan :)"; /* type your text here */
+var array = string.split("");
+var timer;
+  
+function frameLooper () {
+  if (array.length > 0) {
+    document.getElementById("typing").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+      }
+  loopTimer = setTimeout('frameLooper()',150); /* change 70 for speed */
+
+}
+frameLooper();
